8be894de11d10d3b8019b0445f16acd514a8f834
Mohammed_Naeem_Patel <patelnaeem140@gmail.com>
2026-05-28 19:27:36 +0530

    Added dark mode and edit feature

--- To-Do List App.js
+++ To-Do List App.js
@@ -1,5 +1,7 @@
 const inputBox=document.getElementById("input-box");
 const listContainer=document.getElementById("list-container");
+const themeBtn=document.getElementById("theme-btn");
+const container=document.querySelector(".container");
 
 function addTask(){
     if(inputBox.value===''){
@@ -9,9 +11,15 @@ function addTask(){
         let li=document.createElement("li");
         li.innerHTML=inputBox.value;
         listContainer.appendChild(li);
-        let span=document.createElement("span");
-        span.innerHTML="\u00d7";
-        li.appendChild(span);
+
+        let editBtn=document.createElement("button");
+        editBtn.innerHTML="🖉";
+        editBtn.classList.add("edit-btn");
+        li.appendChild(editBtn);
+
+        let dltbtn=document.createElement("span");
+        dltbtn.innerHTML="🗑️";
+        li.appendChild(dltbtn);
     }
     inputBox.value="";
     saveData();
@@ -26,7 +34,15 @@ listContainer.addEventListener("click", function(e){
         e.target.parentElement.remove();
         saveData();
     }
-}, false)
+    else if(e.target.classList.contains("edit-btn")){
+        let li=e.target.parentElement;
+        let updatedText=prompt("Edit Your Task:", li.firstChild.textContent);
+        if(updatedText !==null && updatedText.trim()!==''){
+            li.firstChild.textContent=updatedText;
+            saveData();
+        }
+    }
+}, false);
 
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);
@@ -34,4 +50,23 @@ function saveData(){
 function showTask(){
     listContainer.innerHTML=localStorage.getItem("data");
 }
-showTask(); 
+showTask();
+
+
+themeBtn.addEventListener("click", function(){
+    container.classList.toggle("dark-theme");
+    
+    if(container.classList.contains("dark-theme")){
+        themeBtn.innerHTML="🔆";
+        localStorage.setItem("theme","dark");
+    }
+    else{
+        themeBtn.innerHTML="🌙";
+        localStorage.setItem("theme","light")
+    }
+});
+
+if(localStorage.getItem("theme")==="dark"){
+    container.classList.add("dark-theme");
+    themeBtn.innerHTML="🔆";
+}
